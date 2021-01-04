2b1874c59ae0aa298dba7f4c960f71300597a2d0
qiaomingxing <491302297@qq.com>
2021-01-04 15:07:39 +0800

    api

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,6 @@ import request from '../request'
 const modulesFiles = require.context('./modules', true, /\.ts$/)
 // 注册
 modulesFiles.keys().forEach(modulePath => {
-  console.log(modulePath)
   const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
   const value = modulesFiles(modulePath)
   request.reg(moduleName, value.default)
